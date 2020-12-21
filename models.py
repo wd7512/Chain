6618a91a6f2d16a88a0c5f0a35d394047dcfd916
wd7512 <wwdennis.home@gmail.com>
2020-12-21 18:24:27 +0000

    migration to models

--- models.py
+++ models.py
@@ -1,4 +1,10 @@
 import smtplib, ssl
+from datetime import timedelta  ##new import
+from flask_sqlalchemy import SQLAlchemy  ##new import
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import (Flask, render_template, redirect,
+                   url_for, request, session, flash)
+import pandas as pd
 
 
 def send_email(receiver_email,message):
@@ -13,3 +19,57 @@ def send_email(receiver_email,message):
     with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
+
+def setup_db(app):
+
+    # ------------------SETUP---------------------------
+    app.secret_key = "hello"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+    # users is the name of the table we will be referencing
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # remove prints everytime db changes
+    app.permanent_session_lifetime = timedelta(minutes=5)
+    db = SQLAlchemy(app)  # setting new db
+    # --------------DATABASE CLASS-----------------------
+    class database(db.Model):  # inherit db.model attributes #represent user object in database
+        id = db.Column("id", db.Integer, primary_key=True)
+        # defining each object will have an ID, it will be an integer,, and we will reference all objects to this id
+        # every single row will have a different ID
+        name = db.Column(db.String(100))  # 100 char max str
+        email = db.Column(db.String(100), unique=True)  # values can be float, boolean etc...
+        password = db.Column(db.String(100))
+
+    return db
+
+def login_page():
+    if request.method == 'POST':  # if button pressed
+        email = request.form.get('email')
+        password = request.form.get('password')
+        #print("Email: ", email,"Password: ", password)
+        user = database.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return render_template('login.html', title='Homepage')  # if the user doesn't exist or password is wrong, reload the page
+        else:
+            #print("Database Password is: ", user.password)
+            flash('Welcome')
+            return redirect(url_for('home'))
+    return render_template('login.html', title='Homepage')
+
+def signup_page():
+    if request.method == 'POST':  # if button pressed
+        email = request.form.get('email')
+        name = request.form.get('username')
+        password = request.form.get('password')
+        #print(email,name,password) # Prints 1,1,1
+        user = database.query.filter_by(email=email).first()
+        #print(user) # prints None
+        if user:  # if a user is found, we want to redirect back to signup page so user can try again
+            return redirect(url_for('login'))
+        else:
+            new_user = database(email=email, name=name,
+                            password=generate_password_hash(password, method='sha256'))
+            #print(new_user)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('home'))
+    return render_template('signup.html', title='Homepage')
--- website.py
+++ website.py
@@ -1,63 +1,22 @@
 from flask import (Flask, render_template, redirect,
                    url_for, request, session, flash)
 import pandas as pd
-from datetime import timedelta  ##new import
-from flask_sqlalchemy import SQLAlchemy  ##new import
-from werkzeug.security import generate_password_hash, check_password_hash
 
+
+from models import login_page, setup_db, signup_page
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
-# ------------------SETUP---------------------------
-app.secret_key = "hello"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
-# users is the name of the table we will be referencing
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # remove prints everytime db changes
-app.permanent_session_lifetime = timedelta(minutes=5)
-db = SQLAlchemy(app)  # setting new db
-# --------------DATABASE CLASS-----------------------
-class database(db.Model):  # inherit db.model attributes #represent user object in database
-    id = db.Column("id", db.Integer, primary_key=True)
-    # defining each object will have an ID, it will be an integer,, and we will reference all objects to this id
-    # every single row will have a different ID
-    name = db.Column(db.String(100))  # 100 char max str
-    email = db.Column(db.String(100), unique=True)  # values can be float, boolean etc...
-    password = db.Column(db.String(100))
+
+db = setup_db(app)
 
 @app.route('/login', methods=['GET', 'POST'])  # login page
 def login():
-    if request.method == 'POST':  # if button pressed
-        email = request.form.get('email')
-        password = request.form.get('password')
-        #print("Email: ", email,"Password: ", password)
-        user = database.query.filter_by(email=email).first()
-        if not user or not check_password_hash(user.password, password):
-            flash('Please check your login details and try again.')
-            return render_template('login.html', title='Homepage')  # if the user doesn't exist or password is wrong, reload the page
-        else:
-            #print("Database Password is: ", user.password)
-            flash('Welcome')
-            return redirect(url_for('home'))
-    return render_template('login.html', title='Homepage')
+    return login_page()
+    
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':  # if button pressed
-        email = request.form.get('email')
-        name = request.form.get('username')
-        password = request.form.get('password')
-        #print(email,name,password) # Prints 1,1,1
-        user = database.query.filter_by(email=email).first()
-        #print(user) # prints None
-        if user:  # if a user is found, we want to redirect back to signup page so user can try again
-            return redirect(url_for('login'))
-        else:
-            new_user = database(email=email, name=name,
-                            password=generate_password_hash(password, method='sha256'))
-            #print(new_user)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('home'))
-    return render_template('signup.html', title='Homepage')
+    return signup_page()
 
 
 # Create a URL route in our application for "/"
