07a7884dd507a84258c2c829f099e3602acd47bd
Joao Jesus <joaojesusbath@users.noreply.github.com>
2020-12-23 01:12:46 +0000

    users table now has customer_type

--- models.py
+++ models.py
@@ -1,26 +1,26 @@
 import smtplib, ssl
 from datetime import timedelta  ##new import
 from flask_sqlalchemy import SQLAlchemy  ##new import
+from sqlalchemy import Column, String,Integer, Unicode, engine
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import (Flask, render_template, redirect,
                    url_for, request, session, flash)
 import pandas as pd
 import sqlite3
+from alembic import op # For Database migrations
+
 
 def open_table(table_name):
     # Create your connection.
     cnx = sqlite3.connect('database.sqlite3')
-
-    df = pd.read_sql_query('SELECT * FROM '+table_name, cnx)
-
+    df = pd.read_sql_query('SELECT * FROM ' + table_name, cnx)
     return df
 
 
-def send_email(receiver_email,message):
+def send_email(receiver_email, message):
     port = 465  # For SSL
     password = 'bath2020'
     sender_email = 'chain.app.ai@gmail.com'
-    
 
     # Create a secure SSL context
     context = ssl.create_default_context()
@@ -29,19 +29,18 @@ def send_email(receiver_email,message):
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
+
 def setup_db(app):
-    
-    global db #make variables accessible
+    global db  # make variables accessible
     global users
 
-    # ------------------SETUP---------------------------
     app.secret_key = "hello"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
     # users is the name of the table we will be referencing
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # remove prints everytime db changes
     app.permanent_session_lifetime = timedelta(minutes=5)
     db = SQLAlchemy(app)  # setting new db
-    # --------------DATABASE CLASS-----------------------
+
     class users(db.Model):  # inherit db.model attributes #represent user object in users
         id = db.Column("id", db.Integer, primary_key=True)
         # defining each object will have an ID, it will be an integer,, and we will reference all objects to this id
@@ -49,52 +48,59 @@ def setup_db(app):
         name = db.Column(db.String(100))  # 100 char max str
         email = db.Column(db.String(100), unique=True)  # values can be float, boolean etc...
         password = db.Column(db.String(100))
-        
+        customer_type = db.Column(db.String(100))
+
     db.create_all()
 
     return users
+
 
 def login_page():
     if request.method == 'POST':  # if button pressed
         email = request.form.get('email')
         password = request.form.get('password')
-
         user = users.query.filter_by(email=email).first()
         if not user or not check_password_hash(user.password, password):
             flash('Please check your login details and try again.')
-            return render_template('login.html', title='Homepage')  # if the user doesn't exist or password is wrong, reload the page
+            return render_template('login.html',
+                                   title='Homepage')  # if the user doesn't exist or password is wrong, reload the page
         else:
-            #print("Database Password is: ", user.password)
+            # print("Database Password is: ", user.password)
             flash('Welcome')
             return redirect(url_for('home'))
     return render_template('login.html', title='Homepage')
+
 
 def signup_page():
     if request.method == 'POST':  # if button pressed
         email = request.form.get('email')
         name = request.form.get('username')
         password = request.form.get('password')
-        #print(email,name,password) # Prints 1,1,1
+        input_customer_type = request.form.get("customer_type")
+        print(email, name, password, input_customer_type)  # Prints 1,1,1
         user = users.query.filter_by(email=email).first()
-        #print(user) # prints None
+        # print(user) # prints None
         if user:  # if a user is found, we want to redirect back to signup page so user can try again
             return redirect(url_for('login'))
         else:
             new_user = users(email=email, name=name,
-                            password=generate_password_hash(password, method='sha256'))
-            #print(new_user)
+                             password=generate_password_hash(password, method='sha256'),
+                             customer_type=input_customer_type)
+            # print(new_user)
+            flash("New customer: ", f"{email, name, password, input_customer_type}")
             db.session.add(new_user)
             db.session.commit()
             return redirect(url_for('home'))
     return render_template('signup.html', title='Homepage')
+
 
 def forgot_pass():
     if request.method == 'POST':
         email = request.form.get('email')
         user = users.query.filter_by(email=email).first()
         if user:  # if a user is found, we want to redirect back to signup page so user can try again
-            send_email(email,'changepass')
+            send_email(email, 'changepass')
         else:
             return redirect(url_for('passchange'))
-            
+
     return render_template('passchange.html', title='Change Password')
--- website.py
+++ website.py
@@ -1,16 +1,19 @@
 from flask import (Flask, render_template, Markup)
-
-
 from models import login_page, setup_db, signup_page, forgot_pass, open_table
+from alembic import op # For Database migrations
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
-datafram = open_table('users')
 users = setup_db(app)
+datafram = open_table('users')
+
+
+
+
 
 @app.route('/login', methods=['GET', 'POST'])  # login page
 def login():
     return login_page()
-    
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -26,11 +29,13 @@ def home():
 @app.route('/contact')  # contact page
 def contact():
     return render_template('contact.html', title='Contact')
-    
+
+
 @app.route('/auth')  # authentication page
 def auth():
     return render_template('auth.html', title='Auth')
-    
+
+
 @app.route('/passchange', methods=['GET', 'POST'])  # password change page
 def passchange():
     return forgot_pass()
@@ -43,12 +48,10 @@ def test():
 
 @app.route("/database")
 def view():
-    return render_template("database.html", values = Markup(datafram.to_html())) #get all db users and pass them to the template
+    return render_template("database.html",
+                           values=Markup(datafram.to_html()))  # get all db users and pass them to the template
 
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    
     app.run(debug=True)
-
-
