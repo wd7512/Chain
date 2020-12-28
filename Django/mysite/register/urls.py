1009f5b02f676f9aae7900efe81545951873185e
Joao Jesus <joaojesusbath@users.noreply.github.com>
2020-12-28 02:06:30 +0000

    minor change
    ws

--- Django/mysite/register/urls.py
+++ Django/mysite/register/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('register/',views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('', views.login_view, name='home'),
     #path('login/', views.login, name='contact'),
     #path('', include("django.contrib.auth.urls")), #built in login
     #path('signup/', views.signup, name='signup'),
