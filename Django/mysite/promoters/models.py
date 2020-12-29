7d7a3ce69459e03bf67c51f0629579171c85e9f5
wd7512 <wwdennis.home@gmail.com>
2020-12-29 17:36:23 +0000

    Update models.py

--- Django/mysite/promoters/models.py
+++ Django/mysite/promoters/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.forms import extras
 
 # Create your models here.
+
+class init_form(models.Model):
+
+    sex_choices = [('M','Male'),('F','Female'),('O','Other')]
+    
+    username = models.CharField(max_length = 100)
+    instagram_id = models.CharField(max_length = 100)
+    sex = models.CharField(max_length = 1,choices = sex_choices,blank = True)
+    submission_date = models.DateTimeField()
+    birthdate = forms.DateField(widget=extras.SelectDateWidget)
