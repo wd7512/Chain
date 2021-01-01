e61151e5dc5ad8ce929b350763b26907157b4ca6
wd7512 <wwdennis.home@gmail.com>
2021-01-01 19:13:47 +0000

    Update admin.py

--- Django/mysite/register/admin.py
+++ Django/mysite/register/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from register.models import Account
 
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('email',
+                    'first_name',
+                    'last_name',
+                    'date_joined',
+                    'last_login',
+                    'is_admin',)
+
+    search_fields = ('emails','first_name','last_name')
+
+    readonly_fields = ('date_joined','last_login')
+
+    filter_hotizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+    
+admin.site.register(Account, AccountAdmin)
