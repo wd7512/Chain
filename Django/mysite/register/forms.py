e3ecfd68a519c069ffd4e19ce6d99dc22def2467
wd7512 <35834979+wd7512@users.noreply.github.com>
2021-01-06 10:19:38 +0000

    tiny bugfix

--- Django/mysite/register/forms.py
+++ Django/mysite/register/forms.py
@@ -10,7 +10,7 @@ class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = Account
-        fields = ('email', 'username','type_client', 'password1', 'password2', )
+        fields = ('email', 'username', 'password1', 'password2', )
 
 
 class AccountAuthenticationForm(forms.ModelForm):
